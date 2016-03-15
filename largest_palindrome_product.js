ccf5132222f96f7ec57a1ea2b2017d403f3ed537
Taylor Kennedy <taykenned@gmail.com>
2016-03-15 11:40:07 -1000

    adds a method to find the largest palindrome with 2 factors of with specified digit size

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -10,9 +10,20 @@ module.exports = function(digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
 
-  // do your work here
+    var min = Math.pow(10, digits - 1);
+    var max = Math.pow(10, digits);
 
-
+    for(var num = max*max; num > min*min; num--){
+        if(isPalindrome(num)) {
+            var factors = digitFactors(num, digits);
+            if(factors.length === 2) {
+                factor_0 = factors[0];
+                factor_1 = factors[1];
+                palindromeNumber = num;
+                break;
+            }
+        }
+    }
 
   return {
     factor_0 : factor_0,
@@ -20,3 +31,36 @@ module.exports = function(digits){
     palindromeNumber : palindromeNumber
   };
 };
+
+function isPalindrome(num) {
+    var n = num;
+    var rev = 0;
+
+    while(n > 0) {
+        rev = rev * 10 + n % 10;
+        n =Math.floor(n / 10)
+    }
+
+    if(num === rev) {
+        return true;
+    }
+    return false;
+}
+
+function digitFactors(num, digits) {
+    var min = Math.sqrt(num);
+    var max = Math.pow(10, digits) - 1;
+    var factors = []
+
+    for(var i = max; i >= min; i--) {
+        if(num / i > max) {
+            break;
+        }
+
+        if(num % i === 0) {
+            factors[0] = num/i;
+            factors[1] = i;
+        }
+    }
+    return factors;
+}
